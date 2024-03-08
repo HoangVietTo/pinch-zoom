8ff5481fefce5df9b6eb531cbc3611c829812cbd
HoangVietTo <hoang.viet.to@tsk-praha.cz>
2024-03-08 13:02:48 +0100

    just map

--- src/App.js
+++ src/App.js
@@ -1,18 +1,7 @@
-import React, { useCallback, useRef } from "react";
-import QuickPinchZoom, { make3dTransformValue } from "react-quick-pinch-zoom";
+import React from "react";
 import { MapInteractionCSS } from "react-map-interaction";
 
-export default function CombinedZoomAndPan() {
-  const imgRef = useRef();
-
-  const onPinchZoomUpdate = useCallback(({ x, y, scale }) => {
-    const { current: img } = imgRef;
-    if (img) {
-      const value = make3dTransformValue({ x, y, scale });
-      img.style.setProperty("transform", value);
-    }
-  }, []);
-
+export default function App() {
   return (
     <MapInteractionCSS
       showControls
@@ -21,27 +10,17 @@ export default function CombinedZoomAndPan() {
         translation: { x: 0, y: 20 }
       }}
       minScale={0.5}
-      maxScale={10} // Adjust maxScale based on your preference
+      maxScale={3}
       translationBounds={{
         xMax: 400,
         yMax: 200
       }}
     >
-      <QuickPinchZoom
-        onUpdate={onPinchZoomUpdate}
-        tapZoomFactor={2}
-        zoomOutFactor={4}
-        inertiaFriction={0}
-        maxZoom={10}
-        minZoom={0.5}
-      >
-        <img
-          width="50%"
-          ref={imgRef}
-          alt="img"
-          src="https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
-        />
-      </QuickPinchZoom>
+      <img
+        src="https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+        alt="test"
+        width="100%"
+      />
     </MapInteractionCSS>
   );
 }

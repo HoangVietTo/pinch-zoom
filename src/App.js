cb44cb2ca47d10e07ea7f733413cf1a8ac92da14
HoangVietTo <hoang.viet.to@tsk-praha.cz>
2024-03-08 12:39:38 +0100

    fist commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback, useRef } from "react";
+import QuickPinchZoom, { make3dTransformValue } from "react-quick-pinch-zoom";
+import { MapInteractionCSS } from "react-map-interaction";
 
-function App() {
+export default function CombinedZoomAndPan() {
+  const imgRef = useRef();
+
+  const onPinchZoomUpdate = useCallback(({ x, y, scale }) => {
+    const { current: img } = imgRef;
+    if (img) {
+      const value = make3dTransformValue({ x, y, scale });
+      img.style.setProperty("transform", value);
+    }
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MapInteractionCSS
+      showControls
+      defaultValue={{
+        scale: 1,
+        translation: { x: 0, y: 20 }
+      }}
+      minScale={0.5}
+      maxScale={10} // Adjust maxScale based on your preference
+      translationBounds={{
+        xMax: 400,
+        yMax: 200
+      }}
+    >
+      <QuickPinchZoom
+        onUpdate={onPinchZoomUpdate}
+        tapZoomFactor={2}
+        zoomOutFactor={4}
+        inertiaFriction={0}
+        maxZoom={10}
+        minZoom={0.5}
+      >
+        <img
+          width="50%"
+          ref={imgRef}
+          alt="img"
+          src="https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+        />
+      </QuickPinchZoom>
+    </MapInteractionCSS>
   );
 }
-
-export default App;
